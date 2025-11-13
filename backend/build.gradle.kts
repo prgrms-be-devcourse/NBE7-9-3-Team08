2bc70a42139aeb96cd0fb0496df0f19932db94b9
aovor <0hy2s2@naver.com>
2025-11-13 10:41:07 +0900

    init(github): PR 템플릿, 테스트 자동화 추가

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -67,5 +67,10 @@ dependencies {
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform {
+        // CI 환경에서는 Redis 테스트 제외
+        if (System.getenv("CI") == "true") {
+            excludeTags("redis")
+        }
+    }
 }
