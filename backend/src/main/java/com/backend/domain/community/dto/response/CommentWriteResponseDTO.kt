d683a9cc5bb23378f31d1a7c75d325327acff9a1
yhwHUB <yanghw1902@gmail.com>
2025-11-13 15:45:36 +0900

    feat(be/community) : CommentWriteResponseDTO 코틀린 전환

---
+++ backend/src/main/java/com/backend/domain/community/dto/response/CommentWriteResponseDTO.kt
@@ -0,0 +1,18 @@
+package com.backend.domain.community.dto.response
+
+import com.backend.domain.community.entity.Comment
+import java.time.LocalDateTime
+
+data class CommentWriteResponseDTO(
+    val commentId: Long?,
+    val memberId: Long,
+    val comment: String,
+    val createDate: LocalDateTime
+) {
+    constructor(comment: Comment) : this(
+        comment.id,
+        comment.memberId,
+        comment.comment,
+        comment.getCreateDate()
+    )
+}
